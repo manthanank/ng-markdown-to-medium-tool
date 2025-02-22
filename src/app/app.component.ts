6b45c6f5f87f94f7f847df10a8e44feadcb85792
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-02-22 14:49:23 +0530

    add tracking api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MediumComponent } from './components/medium/medium.component';
 import { TextFieldComponent } from './components/text-field/text-field.component';
 import { HalfPageComponent } from "./components/half-page/half-page.component";
 import { AppbarComponent } from "./components/app-bar/app-bar.component";
+import { TrackService } from './services/track.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,12 @@ import { AppbarComponent } from "./components/app-bar/app-bar.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-markdown-to-medium-tool';
+
+  trackService = inject(TrackService);
+
+  ngOnInit(): void {
+    this.trackService.trackProjectVisit(this.title);
+  }
 }
